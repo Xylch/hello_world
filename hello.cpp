92a63926a6afb11c1e3c121add8a2bf212335a54
Brandon Selstad <selstadb@ELABSB138-09.easternct.edu>
2015-09-09 14:41:01 -0400

    Added Request For Name
    Completed Exercise

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main () {
   
-  cout << "Hello Class!\n\n";
-  cout << "Bunch of extra text\n\n";
+  string name;
+  cin >> name;
+  cout << "Hello " << name << ", my name is Brandon\n\n";
 
   return 0;
 }
